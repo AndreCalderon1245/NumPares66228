877c4866975a29e728d51382928dfbeffb5ba35a
End User <End User@DESKTOP-62AKJQO>
2021-02-24 20:33:04 -0600

    Numeros pares hasta el 47

---
+++ src/numpares66228/NumPares66228.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numpares66228;
+
+/**
+ *
+ * @author End User
+ */
+public class NumPares66228 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int i = 0;
+        while (i < 48) {
+            if (i%2==0){
+            System.out.println("i = "+ i);
+                  
+        }
+            i++;
+    }
+    }
+}
