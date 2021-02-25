1d3b6cfeb5661e3153fa58208dcee600486f3e98
End User <End User@DESKTOP-62AKJQO>
2021-02-24 20:40:31 -0600

    Correción de numeros pares hasta el 47

--- src/numpares66228/NumPares66228.java
+++ src/numpares66228/NumPares66228.java
@@ -16,7 +16,7 @@ public class NumPares66228 {
      */
     public static void main(String[] args) {
        int i = 0;
-        while (i < 48) {
+        while (i < 95) {
             if (i%2==0){
             System.out.println("i = "+ i);
                   
